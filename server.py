4d581a4b703bfa0d2aa418c64a3a5d775260e3c4
dbto <jens.lechner.dbto@googlemail.com>
2025-09-20 07:44:02 -0400

    PyLint server.py

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+''' Emotion Detection Flask app deployed to localhost:5000
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +7,9 @@ app = Flask('Emotion Detector')
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    ''' Get text from the HTML interface and runs 
+        emotion detector over it.
+    '''
     text_to_analyse = request.args.get('textToAnalyze')
     result = emotion_detector(text_to_analyse)
 
@@ -13,12 +18,15 @@ def sent_analyzer():
     if dominant_emotion is None:
         return 'Invalid text! Please try again!'
 
-    return (f"For the given statement, the system response is 'anger': {result['anger']}, " + 
+    return (f"For the given statement, the system response is 'anger': {result['anger']}, " +
              f"'disgust': {result['disgust']}, 'fear': {result['fear']}, 'joy': {result['joy']} " +
-             f"and 'sadness': {result['sadness']}. The dominant emotion is <b>{dominant_emotion}</b>.")
+             f"and 'sadness': {result['sadness']}. " +
+             f"The dominant emotion is <b>{dominant_emotion}</b>.")
 
 @app.route("/")
 def render_index_page():
+    ''' Renders the HTML interface
+    '''
     return render_template('index.html')
 
 if __name__ == "__main__":
