e8c85e6b48155b903db82e546046e1a199f83d98
dbto <jens.lechner.dbto@googlemail.com>
2025-09-20 06:24:39 -0400

    Add emotion detection

---
+++ emotion_detection.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = { "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock" }
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    response = requests.post(url, json = myobj, headers=header)
+
+    formatted_response = json.loads(response.text)
+
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    highest_score = 0.0
+    dominant_emotion = None
+    for emotion, score in emotions.items():
+        if score >= highest_score:
+            highest_score = score
+            dominant_emotion = emotion
+
+    emotions['dominant_emotion'] = dominant_emotion
+
+    return emotions
